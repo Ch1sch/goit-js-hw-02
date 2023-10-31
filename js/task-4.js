0f764e74c1f3b8f0bccb77b862ea70ed9b8fed1b
Ch1sch <141764993+Ch1sch@users.noreply.github.com>
2023-10-31 18:16:51 +0200

    fix 4 task

--- js/task-4.js
+++ js/task-4.js
@@ -18,9 +18,9 @@ function getShippingCost(country) {
 
 
 
-console.log(getShippingCost("Australia")); 
-console.log(getShippingCost("Germany")); 
-console.log(getShippingCost("China")); 
-console.log(getShippingCost("Chile")); 
-console.log(getShippingCost("Jamaica")); 
-console.log(getShippingCost("Sweden"));  
+console.log(getShippingCost("Australia"));
+console.log(getShippingCost("Germany"));
+console.log(getShippingCost("China"));
+console.log(getShippingCost("Chile"));
+console.log(getShippingCost("Jamaica"));
+console.log(getShippingCost("Sweden")); 
